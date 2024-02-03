ba642433a04b78da8465da17b8632348cddbef44
StevenJBurns <StevenJBurns@users.noreply.github.com>
2024-02-03 00:32:24 -0700

    Update ApplicationSingleInstance.cs

--- WPF/Application/ApplicationSingleInstance.cs
+++ WPF/Application/ApplicationSingleInstance.cs
@@ -3,33 +3,35 @@ using System.Reflection;
 using VB = Microsoft.VisualBasic.ApplicationServices;
 
 namespace Ara.SchematicVisualizer
-	{
-	public class ApplicationSingleInstance : VB.WindowsFormsApplicationBase
+{
+  public class ApplicationSingleInstance : VB.WindowsFormsApplicationBase
+  {
+    private Application app;
+
+    public ApplicationSingleInstance()
+    {
+      this.IsSingleInstance = true;
+    }
+
+    protected override bool OnStartup(VB.StartupEventArgs eventArgs)
 		{
-		private Application app;
-
-		public ApplicationSingleInstance()
-			{ this.IsSingleInstance = true; }
-
-		protected override bool OnStartup(VB.StartupEventArgs eventArgs)
-			{
 			app = new Application();
 			var vmMain = new ViewModel.ApplicationViewModel();
 
 			View.WindowMain winmain = new View.WindowMain()
 				{
-				//Title = "PFT v" + Assembly.GetExecutingAssembly().GetName().Version.ToString(),
+				// Title = "PFT v" + Assembly.GetExecutingAssembly().GetName().Version.ToString(),
 				DataContext = vmMain
 				};
 
 			app.Run(winmain);
 			return false;
-			}
+		}
 
 		protected override void OnStartupNextInstance(VB.StartupNextInstanceEventArgs eventArgs)
-			{
+		{
 			eventArgs.BringToForeground = true;
 			app.MainWindow.Activate();
-			}
-		}
-	}
+    }
+  }
+}
