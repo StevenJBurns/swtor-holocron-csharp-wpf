92df36b233fe21b8c2fc1701b4626fa8f8188374
StevenJBurns <StevenJBurns@users.noreply.github.com>
2024-02-03 00:27:18 -0700

    Update ApplicationEntry.cs

--- WPF/Application/ApplicationEntry.cs
+++ WPF/Application/ApplicationEntry.cs
@@ -1,14 +1,14 @@
 using System;
 
 namespace Ara.SchematicVisualizer
-	{
-	class ApplicationEntry
-		{
-		[STAThread]
-		public static void Main(String[] args)
-			{
-			ApplicationSingleInstance singleinstanceapp = new ApplicationSingleInstance();
-			singleinstanceapp.Run(args);
-			}
-		}
-	}
+{
+  class ApplicationEntry
+  {
+    [STAThread]
+    public static void Main(String[] args)
+    {
+      ApplicationSingleInstance singleinstanceapp = new ApplicationSingleInstance();
+      singleinstanceapp.Run(args);
+    }
+  }
+}
